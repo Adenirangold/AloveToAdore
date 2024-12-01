03fe512514e2f925e30f35518e50c37ac9bd7550
MacBookPro <adeniranbayogold@gmail.com>
2024-12-01 20:05:52 +0100

    Styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,16 +5,15 @@ function Hero() {
     <div className="relative h-screen  w-screen overflow-x-hidden">
       <div>
         <div>
-          <h1>A Love To Adore</h1>
+          <h1 className="font-cinzel font-bold text-3xl">A Love To Adore</h1>
         </div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2   h-50 w-50">
           <img src="/images/alovetoadore-3.jpg" />
         </div>
         <div className="absolute bottom-0 right-2">
-          <h1>
-            precious
-            <br></br>and
-            <br></br>ore
+          <h1 className="font-cinzel font-bold text-3xl sm:text-4xl">
+            <span>precious </span>
+            <span>oreoluwa</span>
           </h1>
         </div>
       </div>
